5f8c00cb2f2ad718b43e99b531ceb31e08e91b3b
Oana Ciocan <oana.ciocan@totalsoft.ro>
2020-08-19 15:50:33 +0300

    variabila blana

--- ConferencePlanner/ConferencePlanner.API/Startup.cs
+++ ConferencePlanner/ConferencePlanner.API/Startup.cs
@@ -40,6 +40,7 @@ namespace ConferencePlanner.Api
                 });
 
             services.AddScoped<IGetDemoRepository, GetDemoRepository>();
+            int onush = 1308;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
