c91ec383a3ce6e1e66410169a95d9f3a24afb70b
Miruna Cojocaru <miruna.cojocaru@totalsoft.ro>
2020-08-21 14:26:00 +0300

    Button Color

--- ConferencePlanner/ConferencePlanner.WinUi/MainForm.Designer.cs
+++ ConferencePlanner/ConferencePlanner.WinUi/MainForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace ConferencePlanner.WinUi
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConferencePlanner.WinUi
 {
     partial class MainForm
     {
@@ -28,6 +31,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -55,6 +59,7 @@
             // 
             // tabControl1
             // 
+            this.tabControl1.AccessibleName = "";
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Location = new System.Drawing.Point(-2, 0);
@@ -65,6 +70,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage1.BackgroundImage")));
             this.tabPage1.Controls.Add(this.dataGridView1);
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.label1);
@@ -75,12 +81,13 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(796, 425);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Spectator";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ConferenceName,
@@ -97,6 +104,7 @@
             this.dataGridView1.Size = new System.Drawing.Size(790, 248);
             this.dataGridView1.TabIndex = 5;
             this.dataGridView1.Text = "dataGridView1";
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // ConferenceName
             // 
@@ -152,6 +160,7 @@
             this.WithdrawButton.HeaderText = "WithdrawButton";
             this.WithdrawButton.Name = "WithdrawButton";
             this.WithdrawButton.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            changeColor();
             // 
             // label2
             // 
@@ -196,7 +205,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(796, 425);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Organizer";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // listBox1
@@ -234,6 +243,32 @@
 
         }
 
+        private void changeColor()
+        {
+            // 
+            // Button color
+            // 
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                DataGridViewButtonCell bc = ((DataGridViewButtonCell)dataGridView1.Rows[i].Cells[6]);
+                bc.FlatStyle = FlatStyle.Flat;
+                bc.Style.BackColor = System.Drawing.Color.Green;
+                bc.Style.ForeColor = System.Drawing.Color.DarkGreen;
+            }
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                DataGridViewButtonCell bc = ((DataGridViewButtonCell)dataGridView1.Rows[i].Cells[7]);
+                bc.FlatStyle = FlatStyle.Flat;
+                bc.Style.BackColor = System.Drawing.Color.Red;
+                bc.Style.ForeColor = System.Drawing.Color.DarkRed;
+
+                DataGridViewButtonCell bc1 = ((DataGridViewButtonCell)dataGridView1.Rows[i].Cells[8]);
+                bc1.FlatStyle = FlatStyle.Flat;
+                bc1.Style.BackColor = System.Drawing.Color.Red;
+                bc1.Style.ForeColor = System.Drawing.Color.DarkRed;
+            }
+        }
+
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
@@ -257,4 +292,5 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
     }
+
 }
--- ConferencePlanner/ConferencePlanner.WinUi/MainForm.cs
+++ ConferencePlanner/ConferencePlanner.WinUi/MainForm.cs
@@ -14,5 +14,65 @@ namespace ConferencePlanner.WinUi
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn)
+            {
+                bool isAttend = false;
+                bool isJoin = false;
+                bool inWithdraw = false;
+                int rowindex = senderGrid.CurrentCell.ColumnIndex;
+                if(rowindex.ToString().Equals("6") && isAttend== false)
+                {
+                    isAttend = true;
+                    pressButtonGreen(sender, e.RowIndex, e.ColumnIndex);
+                    InitTimer(sender,e, e.RowIndex, e.ColumnIndex);
+                }
+                if (rowindex.ToString().Equals("7") && isAttend == false)
+                {
+                    isJoin = true;
+                    pressButtonGreen(sender,e.RowIndex, e.ColumnIndex);
+                }
+                if (rowindex.ToString().Equals("8") && isAttend == false)
+                {
+                    inWithdraw = true;
+                    pressButtonGreen(sender, e.RowIndex, e.ColumnIndex);
+                }
+            }
+        }
+
+        private void pressButtonGreen(object sender,int row, int col)
+        {
+            var senderGrid = (DataGridView)sender;
+            DataGridViewButtonCell bc = ((DataGridViewButtonCell)senderGrid.Rows[row].Cells[col]);
+            bc.FlatStyle = FlatStyle.Flat;
+            bc.Style.BackColor = Color.Red;
+            bc.Style.ForeColor = Color.DarkRed;
+        }
+
+        private void makeButtonGreen(object datagrid, int row, int col)
+        {
+            var senderGrid = (DataGridView)datagrid;
+            DataGridViewButtonCell bc = ((DataGridViewButtonCell)senderGrid.Rows[row].Cells[col]);
+            bc.FlatStyle = FlatStyle.Flat;
+            bc.Style.BackColor = Color.Green;
+            bc.Style.ForeColor = Color.DarkGreen;
+        }
+
+        private Timer timer1;
+        public void InitTimer(object sender, EventArgs e, int row, int col)
+        {
+            timer1 = new Timer();
+            timer1.Tick += (sender,e) => timer1_Tick(sender, e, row, col);
+            timer1.Interval = 10000; // 10 seconds / 10000 MillSecs
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e, int row, int col)
+        {
+            var senderGrid = (DataGridView)sender;
+        }
     }
 }
