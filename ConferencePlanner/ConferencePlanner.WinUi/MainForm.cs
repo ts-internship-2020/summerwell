edf1c924a7cd80794c6f6337bf0d572df854bd34
Miruna Cojocaru <miruna.cojocaru@totalsoft.ro>
2020-08-25 17:59:48 +0300

    City Tab

--- ConferencePlanner/ConferencePlanner.WinUi/MainForm.cs
+++ ConferencePlanner/ConferencePlanner.WinUi/MainForm.cs
@@ -49,7 +49,7 @@ namespace ConferencePlanner.WinUi
             if (x == null || x.Count() == 0)
             {
                 return;
-            }s
+            }
             populateConferenceGridViewByDate(0, 5, dateTimePicker2.Value, dateTimePicker1.Value);
             changeColor();
 
