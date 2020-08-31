68e3671f22308085410590e06be422eb73666c2b
czabava <czabava@TS1853>
2020-08-31 16:34:33 +0300

    Configurari .NET Core

--- ConferencePlanner/ConferencePlanner.API/Startup.cs
+++ ConferencePlanner/ConferencePlanner.API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using ConferencePlanner.Api.Swagger;
+using Microsoft.OpenApi.Models;
 
 
 namespace ConferencePlanner.Api
@@ -40,7 +42,8 @@ namespace ConferencePlanner.Api
                 });
             //comentariu degeaba
             services.AddScoped<IGetDemoRepository, GetDemoRepository>();
-            int onush = 1308;
+
+            services.AddSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +68,22 @@ namespace ConferencePlanner.Api
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger(c =>
+            {
+                c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+                {
+                    swaggerDoc.Servers = new List<OpenApiServer>
+                    {
+                        new OpenApiServer {Url = $"http://{httpReq.Host.Value}{httpReq.PathBase.Value}"}
+                    };
+                });
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Conference Planner API");
             });
         }
     }
