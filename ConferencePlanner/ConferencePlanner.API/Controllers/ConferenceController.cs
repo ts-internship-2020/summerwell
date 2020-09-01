d447cebf1607f1a2ac2c642621891544a6d47bdb
stefan.pogonaru <stefan.pogonaru@TS1340>
2020-09-01 16:22:40 +0300

    controller

---
+++ ConferencePlanner/ConferencePlanner.API/Controllers/ConferenceController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConferencePlanner.Api.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ConferencePlanner/ConferencePlanner.RepositoryEf/Repository/GetDemoRepository.cs
+++ ConferencePlanner/ConferencePlanner.RepositoryEf/Repository/GetDemoRepository.cs
@@ -1,27 +1,34 @@
 ﻿using ConferencePlanner.Abstraction.Model;
 using ConferencePlanner.Abstraction.Repository;
 using ConferencePlanner.Repository.Ef.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace ConferencePlanner.Repository.Ef.Repository
 {
     public class GetDemoRepository : IGetDemoRepository
     {
-        private readonly neverseaContext _dbContext;
+        private readonly summerwellContext _dbContext;
 
-        public GetDemoRepository(neverseaContext dbContext)
+        public GetDemoRepository(summerwellContext dbContext)
         {
             _dbContext = dbContext;
         }
 
         public List<DemoModel> GetDemo(string name)
         {
-            List<Demo> demos = _dbContext.Demo.ToList();
+            List<Conference> conferences = _dbContext.Conference.Include(x => x.ConferenceType).ToList();
+            List<DemoModel> demoModels = conferences.Select(a => new DemoModel() { Id = a.ConferenceId, Name = a.ConferenceType.DictionaryConferenceTypeName }).ToList();
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync("http://localhost:2794/api/Home/{DemoName}");
+           // string content = response.Content.ReadAsStringAsync();
 
-            List<DemoModel> demoModels = demos.Select(a => new DemoModel() { Id = a.Id, Name = a.Name }).ToList();
+
 
             return demoModels;
         }
