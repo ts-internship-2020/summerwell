4084b906d8ea150728d37a3910ad3dc021b5de89
Oana Ciocan <oana.ciocan@totalsoft.ro>
2020-09-02 13:54:04 +0300

    buna

---
+++ ConferencePlanner/ConferencePlanner.API/Controllers/CountryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferencePlanner.Abstraction.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ConferencePlanner.Api.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly ILogger<CountryController> _logger;
+        private readonly IDictionaryCountryRepository _countryRepository;
+        public CountryController(ILogger<CountryController> logger, IDictionaryCountryRepository countryRepository)
+        {
+            _logger = logger;
+            _countryRepository = countryRepository;
+        }
+
+    }
+}
--- ConferencePlanner/ConferencePlanner.API/Swagger/SwaggerExtension.cs
+++ ConferencePlanner/ConferencePlanner.API/Swagger/SwaggerExtension.cs
@@ -34,6 +34,7 @@ namespace ConferencePlanner.Api.Swagger
                     typeof(HomeController).Assembly,
                     typeof(CityController).Assembly,
                     typeof(DictionaryConferenceTypeController).Assembly,
+                    typeof(CountryController).Assembly
                     
                 };
                  
