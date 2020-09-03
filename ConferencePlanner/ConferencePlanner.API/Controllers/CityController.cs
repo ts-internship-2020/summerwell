387a64de1df1a58c4756310c8fc6cd4ca1f3182c
Miruna Cojocaru <miruna.cojocaru@totalsoft.ro>
2020-09-03 14:34:36 +0300

    webview

--- ConferencePlanner/ConferencePlanner.API/Controllers/CityController.cs
+++ ConferencePlanner/ConferencePlanner.API/Controllers/CityController.cs
@@ -58,14 +58,5 @@ namespace ConferencePlanner.Api.Controllers
             _cityRepository.DeleteCity(CityId, IsRemote);
             return Ok();
         }
-
-
-        [HttpPut]
-        [Route("DictionaryCity/EditCity")]
-        public IActionResult EditCity(string Code, string Name, int CityId)
-        {
-            _cityRepository.EditCity(Code, Name, CityId);
-            return Ok();
-        }
     }
 }
